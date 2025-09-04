bce2fe82919744bd8aa63eb92c59e721d15349ed
lavzd13 <lavzd13@gmail.com>
2025-09-04 22:27:21 +0200

    Fixed database script to not run in the background

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-
 set -e
 
-mysqld_safe &
-
-until mysqladmin ping -h localhost --silent; do
-	sleep 1
-done
-
-mysql -u root -p"$MARIADB_ROOT_PASSWORD" <<EOF
+cat > /tmp/init.sql <<EOF
 CREATE DATABASE IF NOT EXISTS ${MARIADB_NAME};
 CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MARIADB_NAME}.* TO '${MARIADB_USER}'@'%';
 FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
 EOF
 
-mysqladmin -u root -p"$MARIADB_ROOT_PASSWORD" shutdown
-
-exec mysqld_safe
+exec mysqld_safe --init-file=/tmp/init.sql
