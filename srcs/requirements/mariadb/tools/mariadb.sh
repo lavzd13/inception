66c441c810d38e068011be68d6f667eba134cb7c
jovan <lavzd13@gmail.com>
2025-09-06 14:46:33 +0200

    Removed unnecessary folder and modified mariadb.sh

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ ! -d "${DATADIR}/mysql" ]; then
+if [ ! -d "/var/lib/mysql" ]; then
 	cat > /tmp/init.sql <<EOF
 	CREATE DATABASE IF NOT EXISTS ${MARIADB_NAME};
 	CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+	CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
 	GRANT ALL PRIVILEGES ON ${MARIADB_NAME}.* TO '${MARIADB_USER}'@'%';
 	FLUSH PRIVILEGES;
-	CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
 EOF
 	exec mysqld_safe --init-file=/tmp/init.sql
 else
